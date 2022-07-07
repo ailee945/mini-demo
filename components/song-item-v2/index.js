a322d71d161fbf7a8f9a7c86a59c0450db3e0de1
Ailee <ailee945@outlook.com>
2022-07-07 22:52:10 +0800

    detail-search-done

--- components/song-item-v2/index.js
+++ components/song-item-v2/index.js
@@ -1,30 +1,24 @@
 // components/song-item-v2/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
     index: {
       type: Number,
       value: 0
     },
-    title: {
-      type: String,
-      value: ''
+    item: {
+      type: Object,
+      value: {}
     }
   },
-
-  /**
-   * 组件的初始数据
-   */
   data: {
 
   },
-
-  /**
-   * 组件的方法列表
-   */
   methods: {
-
+    handleSongItemClick() {
+      const id = this.properties.item.id
+      wx.navigateTo({
+        url: '/pages/music-player/index' + id,
+      })
+    }
   }
 })
--- service/api_search.js
+++ service/api_search.js
@@ -5,3 +5,16 @@ import {
 export function getSearchHot() {
   return cusRequest.get('/search/hot')
 }
+
+export function getSearchSuggest(keywords) {
+  return cusRequest.get('/search/suggest', {
+    keywords,
+    type: 'mobile'
+  })
+}
+
+export function getSearchResult(keywords) {
+  return cusRequest.get('/search', {
+    keywords
+  })
+}
